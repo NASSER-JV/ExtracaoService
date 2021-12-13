a2dbc953ae14cf34474bfd82642a0efbeba862e5
João Vitor Goncalves Pina <joaovitorpinag@hotmail.com>
2021-12-13 00:08:47 -0300

    Colocado o arquivo json de configuracoes para ser opcional
    Signed-off-by: João Vitor Goncalves Pina <joaovitorpinag@hotmail.com>

--- Infrastructure/Configurations/Configuration.cs
+++ Infrastructure/Configurations/Configuration.cs
@@ -4,6 +4,7 @@ namespace ExtracaoService.Infrastructure.Configurations;
 
 public static class Configuration
 {
-    public static readonly IConfigurationRoot Config = new ConfigurationBuilder().AddEnvironmentVariables().AddJsonFile("appsettings.json")
+    public static readonly IConfigurationRoot Config = new ConfigurationBuilder().AddEnvironmentVariables()
+        .AddJsonFile("appsettings.json", true)
         .Build();
 }
